cad790934d1c6f9d2af09dcef3718c8d41e0a1ee
lovelyturtles <kihailes@myumanitoba.ca>
2023-06-15 17:24:53 -0500

    Updated machine learning model

---
+++ FastAPI/BinMap.py
@@ -0,0 +1,8 @@
+class_lab =['battery', 'brown-glass-bottles&jars', 'brown-glass-trash', 
+            'cardboard&paper-bags', 'cardboard-boxes', 'cardboard-cartons', 
+            'clothes', 'electronic-waste ', 'food-scraps', 'green-glass-bottles&jars', 
+            'green-glass-trash', 'metal-trash', 'paper-books-loose-envelopes-etc', 
+            'paper-cups', 'paper-eggcarton-coffeecuptray', 'paper-napkins-towels', 
+            'plastic-bags', 'plastic-bottle ', 'plastic-container', 'plastic-trash', 
+            'shoes', 'tin-and-aluminium-cans-nonhazardous-aerosol', 'trash', 'white-glass-bottles&jars', 
+            'white-glass-trash', 'yard-waste']
